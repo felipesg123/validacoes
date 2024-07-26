df78dcaf858c1982feeb1992d8a327b67dcd69fb
felipesg123 <goncalves.silva@alunoescolassociais.org.br>
2024-07-26 07:20:07 -0300

    add full code

---
+++ email.js
@@ -0,0 +1,27 @@
+// CÓDGO DE VALIDAÇÃO DO EMAIL
+// ----------------------------------------------------------
+function checarEmail(){
+    if(document.forms[0].email.value == "" ||
+       document.forms[0].email.value.indexOf('@') == -1 ||
+       document.forms[0].email.value.indexOf('.') == -1){
+        alert("Por favor, informar um e-mail válido");
+        return false;
+    }else{
+            alert("Email informado");
+            document.getElementById('email').innerHTML = document.forms[0].email.value;
+        }
+    }
+     
+     
+     
+     
+     
+    // ----------------------------------------------------------
+     
+     
+    // CÓDIGO DE VERIFICAÇÃO DO EMAIL DIGITADO
+    // ----------------------------------------------------------
+     
+     
+     
+    // ----------------------------------------------------------
